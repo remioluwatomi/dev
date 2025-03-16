dc2279b18adec2be53266174009c3ccfe5caed65
remy_oluwatomi <remioluwatomicephas@gmail.com>
2025-03-16 03:50:53 -0400

    cd: included files and configs for deployment

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  output: "export",
+  basePath: "/dev"
+};
+
+export default nextConfig;
